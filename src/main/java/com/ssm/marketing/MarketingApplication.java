39df6d5258bcad788133c58b4477593666a9e484
wangjunzhuang <453195106wjz>
2018-10-29 20:26:26 +0800

    第一次提交

---
+++ src/main/java/com/ssm/marketing/MarketingApplication.java
@@ -0,0 +1,12 @@
+package com.ssm.marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingApplication.class, args);
+    }
+}
